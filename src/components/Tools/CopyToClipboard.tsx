086dc1058e41ed91cd2599755e127b8d26377a93
Ttebify <ttebify@gmail.com>
2022-04-03 14:20:45 +0100

    Optionaly disable copying to clipboard

--- src/components/Tools/CopyToClipboard.tsx
+++ src/components/Tools/CopyToClipboard.tsx
@@ -1,21 +1,21 @@
-import React, { useRef, useState } from "react";
-import { FaRegClipboard } from "react-icons/fa";
-import SolidButton from "../Buttons/SolidButton";
+import React, { useCallback, useRef, useState } from "react";
 
 interface CopyToClipboardProps {
   title: string;
   content: string;
+  canCopy?: boolean;
 }
 export default function CopyToClipboard({
   title,
   content,
+  canCopy = true,
 }: CopyToClipboardProps) {
   const [copied, setCopied] = useState(false);
   const codeElement = useRef<HTMLPreElement>(null);
 
-  const copyAddress = () => {
+  const copyAddress = useCallback(async () => {
     const text = codeElement.current?.textContent;
-    if (text) {
+    if (text && canCopy && typeof window !== "undefined") {
       navigator.clipboard
         .writeText(text)
         .then(() => {
@@ -28,23 +28,25 @@ export default function CopyToClipboard({
           setCopied(false);
         });
     }
-  };
+  }, [codeElement, canCopy]);
 
   return (
     <div className="relative w-full my-3 border-b-2 border-gray-200">
       <div className="bg-gray-200 px-4 py-1 flex justify-between items-center">
         <div className="inline-block text-base">{title}</div>
-        <button
-          onClick={copyAddress}
-          className="text-base font-medium underline text-blue-600 inline-block float-right"
-        >
-          {copied ? "Copied!" : "Click to copy"}
-        </button>
+        {canCopy && (
+          <button
+            onClick={copyAddress}
+            className="text-base font-medium underline text-blue-600 inline-block float-right"
+          >
+            {copied ? "Copied!" : "Click to copy"}
+          </button>
+        )}
       </div>
       <pre
         className="w-full text-sm md:text-base p-4 overflow-x-auto"
         ref={codeElement}
-        onClick={copyAddress}
+        onClick={canCopy ? copyAddress : () => {}}
       >
         {content}
       </pre>
